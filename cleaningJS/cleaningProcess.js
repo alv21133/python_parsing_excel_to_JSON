36d76c88dded0280f303618e3cbbc449023ac458
alv21133 <wahyudi.0060@students.amikom.ac.id>
2020-04-10 00:48:39 +0700

    tinggal finising pengukuran

--- cleaningJS/cleaningProcess.js
+++ cleaningJS/cleaningProcess.js
@@ -1,11 +1,19 @@
-let jsonData = require("./pengujianFinal.json");
+var fs = require("fs");
+let jsonData = require("./pengujianOtomotif.json");
 let i = 1;
 
 var sasoriObject = [];
 var bidangObject = [];
+var metodeObject = [];
+var bidangObjectold = [];
+let valuePertama = "";
 let globalkey = "";
+let metodeKey = "";
 let bidangKey = "";
+let DataArray = {};
 var noAkre = "";
+console.log(bidangKey);
+
 function mapping() {
   jsonData["OTO"].forEach((key) => {
     noAkre = key["NO. AKREDITASI"];
@@ -16,8 +24,23 @@ function mapping() {
     telp = key["TELEPON"];
     email = key["EMAIL"];
     lingkup = key["LINGKUP"];
-    let DataArray = {};
-    if (globalkey != noAkre) {
+    bidangPengukuran = key["BIDANG PENGUJIAN"];
+    bahanUji = key["BAHAN ATAU PRODUK YANG DIUJI"];
+    jenisUji = key["JENIS PENGUJIAN ATAU SIFAT-SIFAT YANG DIUKUR"];
+    metodeUji = key["METODE PENGUJIAN, TEKNIK YANG DIGUNAKAN"];
+    keterangan = key["KETERANGAN"];
+
+    // mapping fisrt value
+    // if (globalkey != noAkre) {
+    //   let bidanngArray = {};
+    //   bidanngArray.Bidang = bidangPengukuran;
+    //   bidanngArray.perusahaan = nameLab;
+    //   bidangObject.push(bidanngArray);
+    //   valuePertama = bidangPengukuran;
+    // }
+
+    // MAIN DATA FOR JSON
+    if (keterangan == "--") {
       DataArray.nomorAkreditasi = noAkre;
       DataArray.namaLaboratorium = nameLab;
       DataArray.alamat = alamat;
@@ -26,21 +49,60 @@ function mapping() {
       DataArray.telepon = telp;
       DataArray.email = email;
       DataArray.lingkup = lingkup;
-      DataArray.BidangPengukuran = JSON.stringify(bidangObject)
+      DataArray.BidangPengujian = bidangObject;
       sasoriObject.push(DataArray);
       i++;
       bidangObject = [];
+      DataArray = {};
+    }
+
+    // if (bidangKey != bidangPengukuran) {
+    //   if (valuePertama != bidangPengukuran) {
+    //     let bidanngArray = {};
+    //     bidanngArray.Bidang = bidangPengukuran;
+    //     bidanngArray.perusahaan = nameLab;
+    //     bidangObject.push(bidanngArray);
+    //   }
+    // }
+
+    function dataPengukuran() {
+      let myArray = {};
+      myArray.jenisPengujian = jenisUji;
+      myArray.MetodePengujian = metodeUji;
+      metodeObject.push(myArray);
+    }
+
+    if (i <= 6) {
       console.log(i);
+      if ((i = 6)) {
+        if (bidangKey != bahanUji) {
+          let myArray = {};
+          myArray.Bidang = bidangPengukuran;
+          myArray.bahanYangdiUji = bahanUji;
+          myArray.MetodePengujian = metodeObject;
+          bidangObject.push(myArray);
+          metodeObject = [];
+        }
+      }
+    } else {
+      if (bidangKey != bahanUji) {
+        let myArray = {};
+        myArray.Bidang = bidangPengukuran;
+        myArray.bahanYangdiUji = bahanUji;
+        myArray.MetodePengujian = metodeObject;
+        bidangObject.push(myArray);
+        metodeObject = [];
+      }
     }
-    if (bidangKey != key["BIDANG PENGUJIAN"]) {
-      let bidanngArray = {};
-      bidanngArray.Bidang = key["BIDANG PENGUJIAN"];
-      bidangObject.push(bidanngArray);
-    }
-    bidangKey = key["BIDANG PENGUJIAN"];
-    globalkey = key["NO. AKREDITASI"];
+
+    dataPengukuran();
+    bidangKey = bahanUji;
+    metodeKey = metodeUji;
+    globalkey = noAkre;
+    i++;
   });
 }
 
 mapping();
-console.log(sasoriObject);
+console.log(sasoriObject[0]);
+fs.writeFileSync("result2.json", JSON.stringify(sasoriObject));
